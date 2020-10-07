d5cf9b80000590cfbd0a053be5f746ec65178ae1
thesebonesalone <duikhead2@gmail.com>
2020-10-07 11:57:29 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -9,5 +9,49 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+
   end
+
+  
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+  
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  
+
+  get '/articles/:id' do
+    @article = Article.all.where("id = ?",params[:id])[0]
+    erb :show
+  end
+
+  get "/articles/:id/edit" do
+    @article = Article.all.where("id = ?",params[:id])[0] 
+    erb :edit
+  end
+
+
+  post '/articles' do
+    @id = Article.create(:title => params[:article][:title], :content => params[:article][:content]).id
+    redirect "/articles/#{@id}"
+  end
+
+  patch '/articles/:id' do
+    @article = Article.all.where("id = ?",params[:id])[0]
+    @article.update(:title => params[:article][:title], :content => params[:article][:content])
+    redirect "articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    @article = Article.all.where("id = ?",params[:id])[0]
+    @article.destroy
+  end
+ 
+
 end
